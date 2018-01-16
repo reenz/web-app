71eabeb78aacea3f2c4558eed22270295c214e1f
reena <reena.sanjay.sharma@gmail.com>
2018-01-16 13:09:05 +0000

    evaluating Ruby expressions  using erb

--- app.rb
+++ app.rb
@@ -1,22 +1,6 @@
 require 'sinatra'
-require 'shotgun'
 
-set :session_secret, 'super secret'
-
-get '/Monday' do
-  'Oi!'
-end
-
-get '/route66' do
-  'road trip!'
-end
-
-get '/give_and_go' do
-  'dodge'
-end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
